9c8f7c11187a9fca156b79ae2cbe01596ff368f2
Satpal777 <rsshacker777@gmail.com>
2026-02-08 12:40:09 +0530

    Comp: 01: Implement ticket pricing logic based on age and weekend status

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -23,5 +23,28 @@
  * @returns {number} The ticket price, or -1 for invalid input
  */
 export function getTicketPrice(age, isWeekend) {
-  // Your code here
+
+  // If age is negative or not a number, return -1
+  let price = -1;
+
+  if (Number.isNaN(age) || age < 0) {
+    return price;
+  }
+
+  // Age Groups:
+  if (age <= 12) {
+    price = 8;
+  } else if (age <= 17) {
+    price = 12;
+  } else if (age <= 59) {
+    price = 15;
+  } else if (age >= 60) {
+    price = 10;
+  }
+
+  if(isWeekend){
+    price += 3;
+  }
+
+  return price;
 }
